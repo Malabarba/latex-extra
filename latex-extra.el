962b4e5fefeab3a6961a43f186e17b35f1ad1d57
Artur Malabarba <bruce.connor.am@gmail.com>
2013-10-06 20:14:13 +0100

    Documentation now in source file

--- latex-extra.el
+++ latex-extra.el
@@ -10,8 +10,56 @@
 ;; Separator: /
 
 ;;; Commentary:
-;;
-;; This package defines several extra commands for 
+;; 
+;; Defines extra commands and keys for LaTeX-mode. The additions fall
+;; into the following three categories:
+;; 
+;; # 1-Key Compilation #
+;; 
+;; Tired of hitting `C-c C-c' 4 times (latex, bibtex, latex, view) for
+;; the document to compile? This defines a much needed command that does
+;; **everything** at once, and even handles compilation errors!
+;; 
+;; - `C-c C-a' **=>** `latex/compile-commands-until-done'
+;; 
+;; # Navigating Sections #
+;; 
+;; Five new keybindings are defined for navigating between
+;; sections/chapters. These are meant to be intuitive to people familiar
+;; with `org-mode'.
+;; 
+;; - `C-c C-n' **=>** `latex/next-section'  
+;; Goes forward to the next section-like command in the buffer (\part,
+;; \chapter, \(sub)section, or \(sub)paragraph, whichever comes first).
+;; - `C-c C-p' **=>** `latex/previous-section'  
+;; Same, but goes backward.
+;; - `C-c C-u' **=>** `latex/up-section'  
+;; Goes backward to the previous section-like command containing this
+;; one. For instance, if you're inside a subsection it goes up to the
+;; section that contains it.
+;; - `C-c C-f' **=>** `latex/next-section-same-level'  
+;; Like next-section, except it skips anything that's "lower-level" then
+;; the current one. For instance, if you're inside a subsection it finds
+;; the next subsection (or higher), skipping any subsubsections or
+;; paragraphs.
+;; - `C-c C-b' **=>** `latex/previous-section-same-level'  
+;; Same, but goes backward.
+;; 
+;; # Whitespace Handling #
+;; 
+;; `latex-extra.el' improves `auto-fill-mode' so that it only applies to
+;; text, not equations. To use this improvement, just activate
+;; `auto-fill-mode' as usual.
+;; 
+;; It also defines a new command:  
+;; 
+;; - `C-c C-q' **=>** `latex/clean-fill-indent-environment'  
+;; Completely cleans up the entire current environment. This involves:
+;; 
+;; 1. Removing extraneous spaces and blank lines.
+;; 2. Filling text (and only text, not equations).
+;; 3. Indenting everything.
+;; 
 
 ;;; Instructions:
 ;;
@@ -57,6 +105,13 @@
   (interactive)
   (customize-group 'latex-extra t))
 
+;;; Implementation
+(defun replace-regexp-everywhere (reg rep &optional start end)
+  "Version of replace-regexp usable in lisp code."
+  (goto-char (or start (point-min)))
+  (while (re-search-forward reg end t)
+    (replace-match rep nil nil)))
+
 ;;; Whitespace cleaning
 (defcustom latex/clean-up-whitespace t
   "What type of whitespace will be erased by the function `latex/clean-fill-indent-environment'.
@@ -93,8 +148,8 @@ Performs the following actions (on current environment):
       (LaTeX-mark-environment)
       (let ((l (region-beginning))
             (r (region-end)))
-        (unless (eq latex/clean-up-whitespace 'lines) (lsp/replace-regexp "  +" " " nil l r))
-        (unless (eq latex/clean-up-whitespace 'spaces) (lsp/replace-regexp "\n\n\n+" "\n\n" nil l r)))))
+        (unless (eq latex/clean-up-whitespace 'lines)  (replace-regexp-everywhere "  +" " "))
+        (unless (eq latex/clean-up-whitespace 'spaces) (replace-regexp-everywhere "\n\n\n+" "\n\n")))))
   (unless (eq latex/no-autofill-environments 'all)
     (save-excursion
       (LaTeX-mark-environment)
