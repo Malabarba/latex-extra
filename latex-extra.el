455b7873de095cbce6aa256f33cf64dba3dbaa29
Artur Malabarba <bruce.connor.am@gmail.com>
2015-09-03 19:48:15 +0100

    Version bumps

--- latex-extra.el
+++ latex-extra.el
@@ -4,7 +4,7 @@
 
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>>
 ;; URL: http://github.com/Bruce-Connor/latex-extra
-;; Version: 1.10
+;; Version: 1.11
 ;; Keywords: tex
 ;; Package-Requires: ((auctex "11.86.1") (cl-lib "0.5"))
 ;;
@@ -158,12 +158,11 @@
 (require 'cl-lib)
 (require 'outline)
 
-(defconst latex-extra-version "1.10" "Version of the latex-extra.el package.")
 (defun latex-bug-report ()
   "Opens github issues page in a web browser. Please send me any bugs you find, and please include your Emacs and latex versions."
   (interactive)
   (message "Your latex-version is: %s, and your emacs version is: %s.\nPlease include this in your report!"
-    latex-extra-version emacs-version)
+           latex-extra-version emacs-version)
   (browse-url "https://github.com/Bruce-Connor/latex-extra/issues/new"))
 (defun latex-extra-customize ()
   "Open the customisation menu in the `latex-extra' group."
